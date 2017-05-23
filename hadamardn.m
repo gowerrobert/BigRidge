331bfb08daf2b3eba58cc4ce65e9c112d0c1d145
cheikhToure <ctoure@ens-cachan.fr>
2017-05-23 18:26:24 +0200

    Add files via upload

--- hadamardn.m
+++ hadamardn.m
@@ -4,19 +4,20 @@ function result = hadamardn(x)
 
 n = length(x);
 m = floor(n/2);
-x1 = x(1:m);
+
+if m > 1
+    
+    x1 = x(1:m);
 x2 = x(m+1:end);
 
 
 
-if m > 1
     
-    vect1 = hadamardn(x1);
-    vect2 = hadamardn(x2);
+     vect1 = hadamardn(x1);
+     vect2 = hadamardn(x2);
     
     % O(nlog(n)) complexity
-    
-    result = [vect1 + vect2; vect1 - vect2];
+       result = [vect1 + vect2; vect1 - vect2];
     
 else
     result = [x(1)+x(2);x(1)-x(2)] ;
--- main.m
+++ main.m
@@ -7,34 +7,47 @@ fs=18; %Fontsize
 fw='Bold'; %FontWeight
 fsa=16; %Fontsize
 
-tab_n = 5:1:15;
+tab_n =5:1:20;
 n=length(tab_n);
 
-tab_s = 1 + tab_n; 
+tab_s = tab_n; 
 
 Tn = zeros(size(tab_n));
 Ts = zeros(size(tab_n));
+T_srht = zeros(size(tab_n));
 
 
 for i = 1:n
     
     N = 2^(tab_n(i));
-    s = tab_s(i);
-    
+   s = tab_s(i);
+   %s=1;
+   
     x = rand(N,1);
-    
-   idx = randi(N,s,1);
-    
+        
+  idx = randi(N,s,1); 
+ % idx=3*ones(N,1);
+   
     tic
-    hadamardn(x);
+     hadamards(x,idx);
+    %a(idx);
     toc
-    Tn(i) = toc
-    
+    Ts(i) = toc;
+   
     tic
-    hadamards(x,idx);
+    a = hadamardn(x);
+    a(idx);
     toc
-    Ts(i) = toc
+    Tn(i) = toc;
+    
+   
+
+    i
 end
+
+%,tab_n,T_srht
+figure(3);
+clf
 
  plot(tab_n,Tn,tab_n,Ts,'Linewidth',lw);
  xlabel('Absciss (in log_2(n) where n is the length of the input)','FontSize',fs,'FontWeight',fw);
@@ -42,3 +55,4 @@ end
  title('Time complexities','FontSize',fs,'FontWeight',fw);
  set(gca,'FontWeight',fw,'FontSize',fsa);
  legend({'Time (hadamardn)','Time (hadamard with s = log_2(n))'},'FontWeight',fw,'FontSize',fsa);
+ 
