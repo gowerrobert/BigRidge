77cffaa3837f65bcfe067b6ceea27e94f9d462f3
Robert Gower <gowerrobert@gmail.com>
2017-06-07 19:22:23 +0200

    stuff

---
+++ BigRidge/src/CG.jl
@@ -0,0 +1,35 @@
+
+
+function boot_CG(prob::Prob,options::MyOptions)
+   # flopsperiter = (prob.n)^2;
+    name = "CG";
+    stepmethod = step_CG
+    method = CGMethod(0.0,name,step_CG,boot_CG,zeros(prob.n),zeros(prob.n),0.0 )
+
+method.flopsperiter = 2*countnz(prob.A)+7*prob.n; #nnz(A) + 7n
+method.r[:] = prob.b;
+method.p[:] = method.r;# A'*(b-A*x);
+method.rr = dot(method.r,method.r); 
+    return method;
+end
+
+function step_CG(prob::Prob, x::Array{Float64}, d::Array{Float64}, options::MyOptions, method::CGMethod)
+Ap = prob.A*method.p;
+alpha = (method.rr)/(dot(method.p,Ap));
+d[:]= alpha*method.p;
+method.r[:] = method.r-alpha*Ap;
+rrnew = dot(method.r,method.r);
+beta = (rrnew)/(method.rr);
+method.rr = rrnew;
+method.p[:] = method.r+beta*method.p;
+#println("r :",method.r)
+#println("p :",method.p)
+#println("rr :",method.rr)   
+end
+        
+        
+        
+        
+        
+        
+        
