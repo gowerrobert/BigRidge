06e99a150a44f3ef261bfa32008699e98bcbae44
ctoure <ctoure@ens-cachan.fr>
2017-06-15 13:36:11 +0000

    rewriting the defineProb function and some chages of the hada on

--- BigRidge/src/defineProb.jl
+++ BigRidge/src/defineProb.jl
@@ -1,4 +1,4 @@
-function defineProb(dataset,method_name)
+function defineProb(dataset)
     
     # defining a prob based on the name of the chosen method and the dataset
 
@@ -6,41 +6,38 @@ X,y  = loadDataset(dataset);
 
 prob = Prob([],[],[],0,0,dataset);
 
-    n = minimum(size(X)); #the number of data points is not the minimum dimension of X.
-p = maximum(size(X));
+    n,p = size(X); 
 lambda = 1/p;
 
-if method_name == "Hadamard"
+# if method_name == "Hadamard"
+#     
+#     N = Int(2.^(1+floor(log2(n)))); # make the dimension of our problem a power of 2
+#     mat = eye(N)*lambda;
+#     mat[1:n,1:n] = X*X'+eye(n)*lambda; #What about X? This doesn't change the dimensions of X.
+#     prob.A = mat;
+#     
+#     vect = zeros(N);
+#     vect[1:n] = X*y;
+#     prob.b = vect;
+#     
+#     prob.xsol = prob.A\prob.b;
+#     prob.lambda = lambda;
+#     
+#     prob.n = N;
+#     
+#     
+#     
+#     
+# else
     
-    N = Int(2.^(1+floor(log2(n)))); # make the dimension of our problem a power of 2
-    mat = eye(N)*lambda;
-    mat[1:n,1:n] = X*X'+eye(n)*lambda; #What about X? This doesn't change the dimensions of X.
-    prob.A = mat;
-    
-    vect = zeros(N);
-    vect[1:n] = X*y;
-    prob.b = vect;
-    
-    prob.xsol = prob.A\prob.b;
-    prob.lambda = lambda;
-    
-    prob.n = N;
-    
-    
-    
-    
-else
-    
-    sX = size(X);
-    prob.n=  sX[1];
-    lambda = 1/sX[2] ;
-    prob.A = X*X'+eye(prob.n)*lambda;
+    prob.n=  n;
+    lambda = 1/p ;
+    prob.A = X*X'+eye(n)*lambda;
     
     prob.b = X*y;
     prob.xsol = prob.A\prob.b;
     prob.lambda = lambda;
     
-end
 
 return prob;
 
--- BigRidge/src/hada.jl
+++ BigRidge/src/hada.jl
@@ -7,25 +7,46 @@ s = length(idx);
 
 m = Int(floor(n/2));
 
+   # idx_new = idx.*(idx.<=m)+(idx-m).*(idx.>m);
 
-if m > 1
+    idx_new = idx - m*(idx .> m); 
+
+
+    
+if n > 3
     
     x1 = x[1:m,:];
     x2 = x[m+1:end,:];
     
     
-    idx_new = idx.*(idx.<=m)+(idx-m).*(idx.>m);
-    #
+    
     vect1 = hada(x1,idx_new);
     vect2 = hada(x2,idx_new);
     
     return vect1+vect2 -2*vect2.*repmat(idx.>m,1,p);
-    
-else
+#     return vect1+vect2 -2*vect2.*broadcast(*,ones(1,p),idx.>m);
+
+
+        
+    elseif n == 3
+        
+        x1 = x[1:2,:];
+        x3 = reshape(x[3,:],1,p);
+            vect1 = hada(x1,idx_new);
+    vect2 = repmat(x3,s,1);
+        
+        return vect1+vect2 -2*vect2.*repmat((idx .== 3),1,p);
+#         return vect1+vect2 -2*vect2.*broadcast(*,ones(1,p),idx .== 3) ;
+
+       
+    else 
      x1 = reshape(x[1,:],1,p);
        x2 = reshape(x[2,:],1,p);
       return repmat(x1+x2,s,1) - 2*repmat(x2,s,1).*repmat(idx-1,1,p);
-    
+#       return repmat(x1+x2,s,1) - 2*repmat(x2,s,1).*broadcast(*,ones(1,p),idx-1);
+
+
+        
 end
 
 end
