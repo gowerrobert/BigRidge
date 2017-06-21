b61dde15ec6016f506db3727bc2c7cf59f6002d5
ctoure <ctoure@ens-cachan.fr>
2017-06-21 15:13:24 +0000

    a :w

--- BigRidge/src/countmin.jl
+++ BigRidge/src/countmin.jl
@@ -13,13 +13,16 @@ function step_countmin(prob::Prob, x::Array{Float64}, options::MyOptions )
 
 s = options.sketchsize;
 ll = sample(1:s, prob.n); # sample n items from [s] with replacement
+sgn = reshape(sample(1:2,prob.n) * 2 - 3, 1,prob.n); # one half are +1 and the rest are −1
     
-b = reshape(prob.b,1,prob.n);
+prob.b = reshape(prob.b,1,prob.n);
 
-as = Countstream(prob.A,s,ll); # A * S
+as = Countstream(prob.A,s,ll,sgn); # A * S
+as = Countstream(prob.b,s,ll,sgn); # b * S
 
+    
 S = prob.A \ as; # It has to be something else;
-vect = as'*x-(b*S)'; 
+vect = as'*x-(bs)'; 
 x[:] = x[:] -S*(S'*as\vect); #solving (S^TAS) y = (S^TAx-S^Tb)
 
 
