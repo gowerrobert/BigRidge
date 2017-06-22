88912b75793a3e318ef1e4059d2f8f1b37266df3
ctoure <ctoure@ens-cachan.fr>
2017-06-22 13:19:00 +0000

    ok

--- BigRidge/src/Hadamard.jl
+++ BigRidge/src/Hadamard.jl
@@ -21,18 +21,16 @@ idx = sample(1:prob.n,s,replace = false);
 
 #sgn = reshape(sample(1:2,prob.n) * 2 - 3,prob.n,1); # one half are +1 and the rest are −1
 
+#M = [prob.A reshape(prob.b,prob.n,1)];
+mat = hada([prob.A reshape(prob.b,prob.n,1)],idx); 
+    sa = mat[:,1:end-1];
+    sb = mat[:,end];
 
+#sa = hada(prob.A,idx);   # S * A
 
+#sb = hada(reshape(prob.b,prob.n,1),idx);
 
-sa = hada(prob.A,idx);   # S * A
-
-#S = hada(eye(prob.n),idx);   # S
-
-
-sb = hada(reshape(prob.b,prob.n,1),idx);
-#sb = S*prob.b; # S * b
-sas = hada(sa',idx);
-#sas = S * sa'; # SAS^T
+ sas = hada(sa',idx);
 
 vect = sa*x-sb;
 y = sas\vect;   # solving (S^TAS) y = (S^TAx-S^Tb)
@@ -41,7 +39,6 @@ y_n = zeros(prob.n,1);
 y_n[idx] = y;
 
 x[:] = x[:] -hada(y_n,1:prob.n);
-#x[:] = x[:] - S'*y;
     
 end
 
