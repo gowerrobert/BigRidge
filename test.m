b0a5f5c73c5abe44f2774c587212adeae9963eb5
Cheikh Toure <tourealite@pwf-client-pro.paris.inria.fr>
2017-08-01 13:27:34 +0200

    ok

--- test.m
+++ test.m
@@ -3,17 +3,32 @@
 
 clear all
 
-n = 4;
-s = 2;
+n = 6;
+s = 3;
 r = nchoosek(n,s);
 idx = nchoosek(1:n,s);
-C  = zeros(n,n);
+C  = zeros(n,n); % represents C^T * C;
 for i=1:r
-    D{i} = zeros(s,n);
+    D{i} = zeros(s,n); %  construction of I_C_i
     ind = idx(i,:);
-    D{i}(1,ind(1)) = 1;
-    D{i}(2,ind(2)) = 1;
-    C = C + D{i}'*D{i};
+    for k = 1:s
+    D{i}(k,ind(k)) = 1;
+    end
+  %  C = C + D{i}'*D{i};
 end
-
-C
+A = zeros(n,n);
+for i = 1:n
+    for j=1:n
+        A(i,j)=j+(i-1)*n;
+    end
+end
+A = A + eye(n); % making A invertible
+B = zeros(s*r,s*r);
+for i=1:r
+    for j=1:r
+        
+        B(1+(i-1)*s:s*i,1+(j-1)*s:s*j) = D{i}*A*D{j}';
+    end
+end
+        
+        
